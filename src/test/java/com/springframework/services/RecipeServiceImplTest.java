30f46b9986842aa2591c46ee22f643411fc1e647
Devender Goyal <goyaldevender@users.noreply.github.com>
2018-05-19 02:51:00 +0530

    Added mockito @Mock test for Service layer

---
+++ src/test/java/com/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.springframework.services;
+
+
+import com.springframework.domain.Recipe;
+import com.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by devender on 19/05/18.
+ */
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    // Recipe repository is a mandatory dependency for service layer
+    // Note: Although its not Autowired, but still its provided at run time by Spring Data JPA
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
