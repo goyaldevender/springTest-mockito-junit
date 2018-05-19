f76a96dafe2b49eb0e52000396b41f899a5ff4bb
Devender Goyal <goyaldevender@users.noreply.github.com>
2018-05-19 10:22:34 +0530

    Added argument captor for controller

--- src/test/java/com/springframework/controllers/IndexControllerTest.java
+++ src/test/java/com/springframework/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.springframework.controllers;
 
+import com.springframework.domain.Recipe;
 import com.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -33,15 +38,30 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
-        // Here we don't need to write the when statement, because we are not doing anything with the model
-        // and whatever we are doing with model can be alternatively tested via verify statements
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        // Argument Captor: To verify what arguments have been passed
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = controller.getIndexPage(model);
 
-        assertEquals("index", viewName);
 
-        // Verifying the interactions and arguments with verify statements
+        //then
+        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 
 }
